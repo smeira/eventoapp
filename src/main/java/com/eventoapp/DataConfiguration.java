0096877a612f3b6e20959a571604f327f26f77b9
smeira <samanta.meira@gmail.com>
2020-02-18 13:04:45 -0300

    primeiro commit, primeira versao

---
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.eventoapp;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@Profile("dev")
+public class DataConfiguration {
+
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventoapp?createDatabaseIfNotExist=true&useSSL=false&useTimezone=true&serverTimezone=GMT-3");
+        dataSource.setUsername("smeira");
+        dataSource.setPassword("SMEIRAsmeira!!2020");
+        return dataSource;
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
---
+++ src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,11 @@
+package com.eventoapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.eventoapp.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, Long>{
+	Evento findByCodigo(long codigo);
+	
+}
+
+
